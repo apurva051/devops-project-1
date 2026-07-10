628da69192cea5f48ff17a2b28d50e897826cf32
apurvapanchal <apurvapanchal103@gmail.com>
2026-07-10 20:00:43 +0530

    Added details to backend/urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,24 +1,32 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("""
+    <html>
+        <head>
+            <title>Device Management System</title>
+        </head>
+        <body style="font-family: Arial; text-align:center; margin-top:80px;">
+            <h1>Welcome to the Device Management System</h1>
+            <p>Select one of the links below:</p>
+
+            <p>
+                <a href="http://135.235.140.0:3000/devices">
+                    Open Device Management Website
+                </a>
+            </p>
+
+            <p>
+                <a href="http://135.235.140.0:8000/api/">
+                    View REST API
+                </a>
+            </p>
+        </body>
+    </html>
+    """)
 
 urlpatterns = [
+    path('', home),
     path('api/', include('devices.urls')),
 ]
